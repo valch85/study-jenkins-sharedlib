e8bd4dea3d43185194dbdbbc16deef7cc491da67
Valentyn Chubukin <val@valch.name>
2021-03-22 18:52:08 +0200

    fix issue with configMap creation

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,7 +2,7 @@ import java.io.*;
 import groovy.io.*;
 
 @NonCPS
-def call(Map config=[""]){
+def call(Map config=[:]){
     def dir = new File(pwd());
 
     new File(dir.path + '/releasenotes.txt').withWriter('utf-8'){
