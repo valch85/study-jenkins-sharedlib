dd572b720e3b43d10e02f6c20988c254a12af0c5
Valentyn Chubukin <val@valch.name>
2021-03-22 20:34:50 +0200

    fix try1

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -4,16 +4,18 @@ import groovy.io.*;
 @NonCPS
 def call(Map config=[:]){
     def dir = new File(pwd());
-
-    new File(dir.path + '/releasenotes.txt').withWriter('utf-8'){
-        writer -> 
-            dir.eachFileRecurse(FileType.ANY){file ->
-                if (file.isDirectory()){
-                    writer.writeLine(file.name);
-                }
-                else{
-                    writer.writeLine(file.name + '\t' + file.length());
-                }
-        }
+    
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+    { 
+    	writer -> 
+                dir.eachFileRecurse(FileType.ANY){ file ->
+    		if (file.isDirectory()){
+    			writer.writeLine(file.name);            
+    		}
+    		else
+    		{
+    			writer.writeLine('\t' + file.name + '\t' + file.length());
+    		}
+           }
     }
 }
