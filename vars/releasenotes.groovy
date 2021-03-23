212a9438849b63514bc0778918c4969cb7fcd6a9
Valentyn Chubukin <val@valch.name>
2021-03-23 13:26:26 +0200

    add data

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,5 +1,8 @@
 import java.io.*;
 import groovy.io.*;
+import java.util.Calendar.*;
+import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
 def call(Map config=[:]){
@@ -17,5 +20,25 @@ def call(Map config=[:]){
     			writer.writeLine('\t' + file.name + '\t' + file.length());
     		}
            }
+    
+        def date = new Date()
+        def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+        writer.writeLine("Date and Time IS: " + sdf.format(date));
+    
+        writer.writeLine("Build Number is: ${BUILD_NUMBER}");
+        
+        def changeLogSets = currentBuild.changeSets;
+        
+        if (config.changes != "false"){
+            for (change in changeLogSets) {
+            	def entries = change.items;
+            	for (entry in entries) {
+            		writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
+            		for (file in entry.affectedFiles) {
+            			writer.writeLine("${file.editType.name} ${file.path}");
+            		}
+            	}
+            }    
+        }    
     }
 }
